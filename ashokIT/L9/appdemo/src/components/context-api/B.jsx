ea68823b5ac0f94700bb397ae7f72d6d4b64df7b
Pratik Jyotirling Bagale <pratikbagale8@gmail.com>
2025-04-16 18:43:22 +0530

    Add Code

---
+++ ashokIT/L9/appdemo/src/components/context-api/B.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import C from './C';
+
+function B() {
+  return (
+    <div style={{padding: "50px"}}>
+      <h2>B Component</h2>
+      < C />
+    </div>
+    
+  )
+}
+
+export default B;
