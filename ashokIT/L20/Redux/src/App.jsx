0b24e01a69e5a4883146818784e1b4384887319a
Pratik Jyotirling Bagale <pratikbagale8@gmail.com>
2025-05-16 23:11:10 +0530

    Add Code

---
+++ ashokIT/L20/Redux/src/App.jsx
@@ -0,0 +1,22 @@
+ import Ref from "./components/ref/Ref"
+ import Timer from "./components/timer/Timer"
+ import RefDom from "./components/ref/RefDom"
+ import UseReducer from "./components/reducer/UseReducer"
+ import Counter from "./components/counter/Counter"
+
+function App() {
+   
+
+  return (
+    <>
+      {/* < Timer />
+      < Ref />  */}
+
+      < RefDom />
+      < UseReducer />
+      < Counter />
+    </>
+  )
+}
+
+export default App;
